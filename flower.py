23330600203e73d6f204c26f39df6dc9c75afa89
sunchichi0505 <sunchichi0505@gmail.com>
2021-09-30 23:10:01 +0900

    flower Update(21.09.30)

--- flower.py
+++ flower.py
@@ -1,17 +1,18 @@
 import turtle as t
+import random
 
 t.speed(0)
-def petal():
+def petal(size, angle):
     for i in range(2):
-        t.circle(150, 110)
-        t.left(70)
+        t.circle(size, angle)
+        t.left(180 - angle)
 
-def draw_flower():
+def draw_flower(petal_num, petal_size, petal_angle):
     t.color("pink")
     t.begin_fill()
-    for i in range(5):
-        petal()
-        t.left(360/5)
+    for i in range(petal_num):
+        petal( petal_size, petal_angle)
+        t.left(360/petal_num)
     t.end_fill()
 
     t.goto(0, -30)
@@ -22,7 +23,11 @@ def draw_flower():
     t.end_fill()
 
 t.bgcolor("ivory")
-draw_flower()
+petal_num = random.randint(4, 10)
+petal_size = random.randint(50, 120)
+petal_angle = random.randint(90, 130)
+
+draw_flower(petal_num, petal_size, petal_angle)
 
 
 
