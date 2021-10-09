3843443a55e0210efb7878b41de2d127d2d8d1b6
sunchichi0505 <sunchichi0505@gmail.com>
2021-10-09 15:55:23 +0900

    news_articles Pull_update(21.10.09)

--- Start Crawling/news_articles.py
+++ Start Crawling/news_articles.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
 response = requests.get("https://search.naver.com/search.naver?where=news&sm=tab_jum&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90")
 html = response.text
@@ -10,4 +11,9 @@ for article in articles:
     links = article.select("a.info") #リスト
     if len(links) >= 2: #リンクが2個以上であれば
         url = links[1].attrs['href'] #二つ目のリンクのhrefを使用
-        print(url)
+        response = requests.get(url, headers={'Users-agent':'Mozila/5.0'})
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        content = soup.select_one("#articleBodyContents")
+        print(content.text)
+        time.sleep(0.3)
