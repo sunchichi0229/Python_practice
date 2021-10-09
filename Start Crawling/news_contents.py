5ccd8fe7f1d629b6879088bbc9b223b3db40d1af
sunchichi0505 <sunchichi0505@gmail.com>
2021-10-09 15:43:11 +0900

    news_article pull(21.10.09)

---
+++ Start Crawling/news_contents.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://search.naver.com/search.naver?where=news&sm=tab_jum&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90")
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+articles = soup.select("div.info_group") #newsの記事 div 10個
+
+for article in articles:
+    links = article.select("a.info") #リスト
+    if len(links) >= 2: #リンクが2個以上であれば
+        url = links[1].attrs['href'] #二つ目のリンクのhrefを使用
+        print(url)
