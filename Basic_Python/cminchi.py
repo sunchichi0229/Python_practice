1942308cba1e1f72dec1c26fa59439bffda7d911
sunchichi0505 <sunchichi0505@gmail.com>
2021-11-07 23:38:33 +0900

    cminchi Update cminchi.py>Front(21.11.07)

--- Basic_Python/cminchi.py
+++ Basic_Python/cminchi.py
@@ -1,1 +1,20 @@
 from tkinter import *
+
+window = Tk()
+window.title("cm/inch変換プログラム")
+window.geometry("250x200")
+window.config(padx=30, pady=10)
+
+label_title = Label(window, text="cm/inch 変換", \
+    font=("游ゴシック",20,"bold"))
+label_title.grid(column=0, row=0, columnspan=2, padx=10, pady=10)
+
+entry_input = Entry(window, width=15)
+entry_input.grid(column=0, row=1, sticky=E)
+
+label_input = Label(window, text="cm", font=("游ゴシック",10,"bold"))
+label_input.grid(column=1, row=1, pady=10, sticky=W)
+
+
+
+window.mainloop()
