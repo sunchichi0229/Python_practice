6151e6813b14143a3487167b68bcc2878018c921
sunchichi0505 <sunchichi0505@gmail.com>
2021-12-10 23:29:45 +0900

    randombox Update>img5-9(21.12.10)

--- Basic_Python/randombox.py
+++ Basic_Python/randombox.py
@@ -10,7 +10,7 @@ canvas.pack()
 
 img_main = PhotoImage(file="images/gift.png")
 
-image_names = ["Nakta","Tokki","Gae","Goyangee","Mal","Yang","Saza","Horangi","Baem","Gorilla", "Dolgorae"]
+image_names = ["Nakta","Tokki","Gae","Goyangee","Mal","Yang","Saza","Horangi","Baem","Gorilla", "Dolgorae","Sanguh"]
 
 
 
