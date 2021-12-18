b0ab217b4368991bd233a929947cffe11d06efd9
sunchichi0505 <sunchichi0505@gmail.com>
2021-12-18 17:40:21 +0900

    [ADD]BeautifulSoup Basic(21.12.18)

--- webscraping_basic/6_bs4.py
+++ webscraping_basic/6_bs4.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://m.stock.naver.com/index.html#/"
+url = "https://finance.naver.com/#/"
 res = requests.get(url)
 res.raise_for_status()
 
@@ -16,7 +16,7 @@ soup = BeautifulSoup(res.text, "lxml")
 
 # print(soup.find("div", attrs={"class":"wbus122"}))
 
-rank1 = soup.find("tr", attrs={"class":"RankingTable_tr__2nw3W"})
+# rank1 = soup.find("tr", attrs={"class":"RankingTable_tr__2nw3W"})
 # print(rank1.a.get_text())
 # print(rank1.next_sibling) #siblingは次の順番に行かせるもの
 # print(rank1.next_sibling.next_sibling) #１回で出てこない場合は2回入力する
@@ -29,5 +29,14 @@ rank1 = soup.find("tr", attrs={"class":"RankingTable_tr__2nw3W"})
 
 # print(rank1.parent) #親オブジェクトが出力される
 
-rank2 = rank1.find_next_sibling("tr") #この場合は自分が探したいtagの値を探せる入力値
-print(rank2.div.get_text())
+# rank2 = rank1.find_next_sibling("tr") #この場合は自分が探したいtagの値を探せる入力値
+# print(rank2.div.get_text())
+
+
+# rank1 = rank2.find_previous_sibling("tr") #この場合は自分が探したいtagの値を探せる入力値
+# print(rank2.div.get_text())
+
+# rank1.find_next_siblings("tr") #siblingの後ろにsをつけることですべての情報を持ってくることが可能
+
+stock = soup.find("a", text="삼성전자")
+print(stock)
